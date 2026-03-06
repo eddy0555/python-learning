5af25ae9628e6d034cd3df61f38288fc4f8a700f
Edward Uriarte <eddyuriarte055@gmail.com>
2026-03-05 21:24:54 -0800

    Adding user input example to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("Hello world. My Python environment works!")
+
+name = input("What is your name? ")
+print("Nice to meet you,", name)
+
+age = input("How old are you? ")
+print("You are", age, "years old.")
