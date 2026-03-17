52c1d22437bb9145d6257115ac50c7b3e3700062
eddy0555 <97937124+eddy0555@users.noreply.github.com>
2026-03-17 20:47:05 +0000

    Adding Psuedo Code

--- 02_control_flow/number_guessing_game.py
+++ 02_control_flow/number_guessing_game.py
@@ -1,3 +1,32 @@
 import random
+#Creating Pseudo code
 
-print("hello")
+"""
+START
+
+Generate a random number between 1 and 100
+Store it as secret_number
+
+Set guess to None (or 0)
+
+Display message: "Guess the number between 1 and 100"
+
+WHILE guess is not equal to secret_number:
+
+    Ask user to enter a guess
+    Convert input to a number
+    Store it in guess
+
+    IF guess is less than secret_number:
+        Display "Too low"
+
+    ELSE IF guess is greater than secret_number:
+        Display "Too high"
+
+    ELSE:
+        Display "Correct! You guessed it"
+
+END WHILE
+
+END
+"""
