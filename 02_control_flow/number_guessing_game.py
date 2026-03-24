5652de0926701b9324440c5ae1d1662dca0fdd46
eddy0555 <97937124+eddy0555@users.noreply.github.com>
2026-03-24 20:50:28 +0000

    Adding a counter for number of guesses

--- 02_control_flow/number_guessing_game.py
+++ 02_control_flow/number_guessing_game.py
@@ -6,32 +6,22 @@ secret_num = random.randint(1,10)
 # Initiate guess variable
 guess_num = None
 
+set_guess = 0
+
 #Prompt number guessing game question
 print ("Try to guess the secret number that is between 1 and 100!")
 
 while guess_num != secret_num:
 
     guess_num = int(input("Enter your guess: "))
+    set_guess += 1
 
     if guess_num < secret_num:
         print ("Too Low bro")
     
-    if guess_num > secret_num:
+    elif guess_num > secret_num:
         print ("Too high bro")
     
-print("Nice! Good Job Stupid")
+    else:print("Nice! Good Job Stupid")
 
-"""
-    IF guess is less than secret_number:
-        Display "Too low"
-
-    ELSE IF guess is greater than secret_number:
-        Display "Too high"
-
-    ELSE:
-        Display "Correct! You guessed it"
-
-END WHILE
-
-END
-"""
+print("Total guesses was:",set_guess)
