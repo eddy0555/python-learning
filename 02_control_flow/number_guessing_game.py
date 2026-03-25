e7f68ebd8624198871394ade116082d2a974bbbd
eddy0555 <97937124+eddy0555@users.noreply.github.com>
2026-03-25 22:23:17 +0000

    Adding try except input handling code.

--- 02_control_flow/number_guessing_game.py
+++ 02_control_flow/number_guessing_game.py
@@ -9,13 +9,18 @@ guess_num = None
 set_guess = 0
 
 #Prompt number guessing game question
-print ("Try to guess the secret number that is between 1 and 100!")
+print ("Try to guess the secret number that is between 1 and 10!")
 
 while guess_num != secret_num:
 
-    guess_num = int(input("Enter your guess: "))
-    set_guess += 1
+    try: 
+        guess_num = int(input("Enter your guess: "))
+        set_guess += 1
 
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+        
     if guess_num < secret_num:
         print ("Too Low bro")
     
